e437d9c64dc5cee15c1f22810118489639001111
valeriesutera <valeriesutera@gmail.com>
2021-01-09 14:39:49 -0500

    adding py files

---
+++ main.py
@@ -0,0 +1,8 @@
+from filereader import file_reader
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    file_name = 'COVID-19_Case_Surveillance_Public_Use_Data.csv'
+    file_reader(file_name)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
