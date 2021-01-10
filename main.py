54593722a5f94fa4609de21ed468c989ce709ecd
valeriesutera <valeriesutera@gmail.com>
2021-01-10 10:58:50 -0500

    updated df functions
    added filter_df and calculator functions

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from filereader import file_reader
+from riskcalculator import filter_df, calculator
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     file_name = 'COVID-19_Case_Surveillance_Public_Use_Data.csv'
-    file_reader(file_name)
+    df = file_reader(file_name)
+    df_filter = df.copy()
+    df_filter = filter_df(df, 'female', '0-9', 'Latino')
+    print("filtered df")
+    print(df_filter)
+    print(calculator(df_filter))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
