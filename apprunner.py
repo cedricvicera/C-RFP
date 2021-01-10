7432303f804c5cd0894bf5968ed51d570ea4ea81
Chenming Cao <62767862+chenming-cao@users.noreply.github.com>
2021-01-10 11:19:17 -0700

    update apprunner to pass user data for processing

--- apprunner.py
+++ apprunner.py
@@ -1,5 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request
+from filereader import file_reader
+from riskcalculator import filter_df, calculator
 
+# Press the green button in the gutter to run the script.
 app = Flask(__name__)
 
 #home page
@@ -14,15 +17,22 @@ def form():
         sex = request.form.get("sex")
         age = request.form.get("age")
         race = request.form.get("race")
-        print(sex, age, race)  #will be replaced to data processing methods
-        return redirect("/") #will be replaced to result page
+        print(sex, age, race)  #will be replaced with data processing methods, delete later
+        df_filter = df.copy()
+        df_filter = filter_df(df, sex, age, race)
+        print("filtered df")
+        print(df_filter)
+        print(calculator(df_filter))
+        return redirect("/") #will be replaced with result page
     else:
         return render_template("form.html")
 
 #result page
-@app.route("/results")
-def results():
-    return render_template("results.html")
+@app.route("/result")
+def result():
+    return render_template("result.html")
 
 if __name__== "__main__":
+    file_name = 'COVID-19_Case_Surveillance_Public_Use_Data.csv'
+    df = file_reader(file_name)
     app.run(debug=True)
