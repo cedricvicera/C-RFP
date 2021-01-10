ae5582343e89433a768776cb3a8417cf7182890c
Chenming Cao <62767862+chenming-cao@users.noreply.github.com>
2021-01-09 21:38:41 -0700

    update apprunner.py and templates
    update form() in apprunner, set correct redirection address; add templates for home, form and result pages.

--- apprunner.py
+++ apprunner.py
@@ -2,17 +2,27 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
+#home page
 @app.route("/")
 def home():
-    return render_template("base.html")
-
-@app.route("/form", methods=["POST", "GET"])
-def getinput():
-    sex = request.form.get("sex")
-    age = request.form.get("age")
-    race = request.form.get("race")
-    print(sex, age, race)
     return render_template("index.html")
+
+#form page: to get user input
+@app.route("/form", methods=["POST", "GET"])
+def form():
+    if request.method == "POST":
+        sex = request.form.get("sex")
+        age = request.form.get("age")
+        race = request.form.get("race")
+        print(sex, age, race)  #will be replaced to data processing methods
+        return redirect("/") #will be replaced to result page
+    else:
+        return render_template("form.html")
+
+#result page
+@app.route("/result")
+def result():
+    return render_template("result.html")
 
 if __name__== "__main__":
     app.run(debug=True)
